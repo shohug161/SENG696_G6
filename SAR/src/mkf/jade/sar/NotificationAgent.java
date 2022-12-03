b07fb385377f91ea736b43871e2ffab2ab76c94f
Amar Gadala <amargadala@gmail.com>
2022-12-02 18:30:26 -0700

    Update to notification java file

--- SAR/src/mkf/jade/sar/NotificationAgent.java
+++ SAR/src/mkf/jade/sar/NotificationAgent.java
@@ -1,34 +1,91 @@
 package mkf.jade.sar;
 
 import jade.core.behaviours.SimpleBehaviour;
+import java.util.Random;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
 
 public class NotificationAgent extends EnhancedAgent{
-
+	 
+	public NotificationAgent() {
+	addBehaviour(new NotificationCommunicator());
 	
-	
+}
 	private class NotificationCommunicator extends SimpleBehaviour {
 
 		private NotificationAgent notificationAgent;
-		private boolean done;
-		
+		private boolean done = false;
+		private short actionCounter = 0;
 		public NotificationCommunicator() {
-			
+			super(NotificationAgent.this);
+			notificationAgent = NotificationAgent.this;
 			
 		}
 		
 		@Override
-		public void action() {
-			// TODO Auto-generated method stub
-			
-			
-			// if ACLMessage.INFORM then the program is done and the agent can be deleted
-		}
+		   public void action() {
+	        ACLMessage msg;
+	        MessageTemplate template;
 
+	        switch(actionCounter) {
+	        					/*   Notification to all the teams */   
+	          case 0:
+	        	  	msg = notificationAgent.blockingReceive();
+	        	  	/* Add Condition */        	  		
+	        	  	  if(msg != null) {
+	        		  	  sendMsg("new task is assigned", Constants.NOTIFICATION, ACLMessage.INFORM, notificationAgent.team);
+	        		  	  System.out.println("Notifcation is sent to all the teams");
+	        		  	  actionCounter = 1;
+	        	  	  }
+	        	  		break;
+	          						/*   Notification to inform that request is cancelled  */   
+	          case 1:
+		            msg = notificationAgent.blockingReceive();
+		            /* Add Condition */   
+		          	  if(msg != null) {
+		          		  sendMsg("Request is cancelled", Constants.REQUEST_CANCELED, ACLMessage.INFORM, notificationAgent.Requester);
+		          		  System.out.println("Software Acquisition request is cancelled");
+		          		  actionCounter = 2;  
+		          	  }
+		          	  	break;
+			  
+	          case 2:
+	        	  					/*   Notification to the vendor  */   
+		            msg = notificationAgent.blockingReceive();
+		            /* Add Condition */   
+			          if(msg != null) {
+			        	  sendMsg("Notification to Vendor",Constants.NOTIFY_VENDOR,ACLMessage.INFORM,notificationAgent.vendor);
+			              System.out.println("Notification to vendor");
+			              actionCounter = 3;  
+			          }
+			            break;       
+	          case 3:
+	        	  						/*   Request complete notification to requester  */   
+	        	  	msg = notificationAgent.blockingReceive();
+	        	  	/* Add Condition */  
+	        	  	  if(msg != null) {
+			        	  sendMsg("Complete notification to Requester",Constants.REQUEST_COMPLETE,ACLMessage.INFORM,notificationAgent.Requester);
+	                   	  System.out.println("Software Acquisition request is complete to the requester");
+			        	  done = true;
+			        	  notificationAgent.doDelete();
+	        	  	  }
+	        	  	  	break;
+	        }
+	      }
+		  
 		@Override
 		public boolean done() {
 			// TODO Auto-generated method stub
 			return done;
 		}
 		
+		
 	}
 }
+
+
+
+
+
+
