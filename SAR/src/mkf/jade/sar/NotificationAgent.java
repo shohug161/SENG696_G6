fbbd369cec1bdb8bf464baa548d183291650511f
Amar Gadala <amargadala@gmail.com>
2022-12-02 23:43:06 -0700

    updates made to the notification file

--- SAR/src/mkf/jade/sar/NotificationAgent.java
+++ SAR/src/mkf/jade/sar/NotificationAgent.java
@@ -1,91 +1,85 @@
 package mkf.jade.sar;
 
+import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.SimpleBehaviour;
 import java.util.Random;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+import mkf.jade.sar.model.RequestInfoModel;
 
+public class NotificationAgent extends EnhancedAgent {
 
-public class NotificationAgent extends EnhancedAgent{
-	 
+	public void handleCompletedRequest(RequestInfoModel completeRequest) {
+		
+		/* send email*/
+		
+	}
 	public NotificationAgent() {
-	addBehaviour(new NotificationCommunicator());
-	
-}
-	private class NotificationCommunicator extends SimpleBehaviour {
+		addBehaviour(new NotificationCommunicator());
+		
+	}
+
+	private class NotificationCommunicator extends CyclicBehaviour {
 
 		private NotificationAgent notificationAgent;
-		private boolean done = false;
-		private short actionCounter = 0;
+		private MessageTemplate requestCompleteTemplate;
 		public NotificationCommunicator() {
 			super(NotificationAgent.this);
 			notificationAgent = NotificationAgent.this;
-			
+			requestCompleteTemplate = createMessageTemplate(Constants.REQUEST_COMPLETE);
 		}
-		
-		@Override
-		   public void action() {
-	        ACLMessage msg;
-	        MessageTemplate template;
 
-	        switch(actionCounter) {
-	        					/*   Notification to all the teams */   
-	          case 0:
-	        	  	msg = notificationAgent.blockingReceive();
-	        	  	/* Add Condition */        	  		
-	        	  	  if(msg != null) {
-	        		  	  sendMsg("new task is assigned", Constants.NOTIFICATION, ACLMessage.INFORM, notificationAgent.team);
-	        		  	  System.out.println("Notifcation is sent to all the teams");
-	        		  	  actionCounter = 1;
-	        	  	  }
-	        	  		break;
-	          						/*   Notification to inform that request is cancelled  */   
-	          case 1:
-		            msg = notificationAgent.blockingReceive();
-		            /* Add Condition */   
-		          	  if(msg != null) {
-		          		  sendMsg("Request is cancelled", Constants.REQUEST_CANCELED, ACLMessage.INFORM, notificationAgent.Requester);
-		          		  System.out.println("Software Acquisition request is cancelled");
-		          		  actionCounter = 2;  
-		          	  }
-		          	  	break;
-			  
-	          case 2:
-	        	  					/*   Notification to the vendor  */   
-		            msg = notificationAgent.blockingReceive();
-		            /* Add Condition */   
-			          if(msg != null) {
-			        	  sendMsg("Notification to Vendor",Constants.NOTIFY_VENDOR,ACLMessage.INFORM,notificationAgent.vendor);
-			              System.out.println("Notification to vendor");
-			              actionCounter = 3;  
-			          }
-			            break;       
-	          case 3:
-	        	  						/*   Request complete notification to requester  */   
-	        	  	msg = notificationAgent.blockingReceive();
-	        	  	/* Add Condition */  
-	        	  	  if(msg != null) {
-			        	  sendMsg("Complete notification to Requester",Constants.REQUEST_COMPLETE,ACLMessage.INFORM,notificationAgent.Requester);
-	                   	  System.out.println("Software Acquisition request is complete to the requester");
-			        	  done = true;
-			        	  notificationAgent.doDelete();
-	        	  	  }
-	        	  	  	break;
-	        }
-	      }
-		  
 		@Override
-		public boolean done() {
-			// TODO Auto-generated method stub
-			return done;
+		public void action() {
+			ACLMessage msg;
+			MessageTemplate template;
+
+			/* Notification to all the teams */
+
+			/* Add Condition */
+			if (msg != null) {
+
+				System.out.println("Notifcation is sent to all the teams");
+
+			}
+
+			/* Notification to inform that request is cancelled */
+
+			/* Add Condition */
+			if (msg != null) {
+
+				System.out.println("Software Acquisition request is cancelled");
+
+				/* Notification to the vendor */
+
+				/* Add Condition */
+				if (msg != null) {
+
+					System.out.println("Notification to vendor");
+					/* Request complete notification to requester */
+				}
+				msg = notificationAgent.receive(requestCompleteTemplate); /* template */
+				/* Add Condition */
+				if (msg != null) {
+					try {
+						RequestInfoModel completed_request = (RequestInfoModel) msg.getContentObject();
+						notificationAgent.handleCompletedRequest(completed_request);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+				block();
+			}
+
 		}
-		
-		
+
+	}
+
+	private MessageTemplate createMessageTemplate(String conversationID) {
+		return MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.REQUEST),
+				MessageTemplate.MatchConversationId(conversationID));
 	}
 }
-
-
-
-
-
-
